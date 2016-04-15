7cf2252925de8cc6f993d1de3e5133415f795148
apneah <apneahh@gmail.com>
2016-04-15 21:34:21 +0200

    add implementation for SimilarityRate class

--- src/SimilarityRate.java
+++ src/SimilarityRate.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimilarityRate {
 
+    private List<Double> similarityRateFromOneMethod;
     private Text text1;
     private Text text2;
 
+
+    public SimilarityRate(Text text1, Text text2) {
+        this.text1 = text1;
+        this.text2 = text2;
+        similarityRateFromOneMethod = new ArrayList<Double>();
+    }
+
+    // simple implementation, returns average rate
+
+    public double computeSimilarityRate() {
+        double result = 0;
+        for(double singleRate : similarityRateFromOneMethod) {
+            result += singleRate;
+        }
+        return result/similarityRateFromOneMethod.size();
+    }
 
 }
